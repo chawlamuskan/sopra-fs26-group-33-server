76f03f6d13a37e11b9e4e02e94437232004a4d0f
nsavas8 <nina.jael@gmx.ch>
2026-04-23 23:26:34 +0200

    Added DTOs for getting all saved places of a user or a travel board #284

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/rest/dto/SavedPlaceGetDTO.java
@@ -0,0 +1,91 @@
+package ch.uzh.ifi.hase.soprafs26.rest.dto;
+
+import java.util.Set;
+
+public class SavedPlaceGetDTO {
+
+    private Long id;
+    private String externalPlaceId;
+    private String name; 
+    private String address;
+    private Double rating;
+    private String photoReference;
+    private Double lat;
+    private Double lng;
+    private Set<String> types;
+    
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getExternalPlaceId() {
+        return externalPlaceId;
+    }
+
+    public void setExternalPlaceId(String externalPlaceId) {
+        this.externalPlaceId = externalPlaceId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+      public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Double getRating() {
+        return rating;
+    }
+    
+    public void setRating(Double rating) {
+        this.rating = rating;
+    }
+
+    public String getPhotoReference() {
+        return photoReference;
+    }
+    
+    public void setPhotoReference(String photoReference) {
+        this.photoReference = photoReference;
+    }
+
+    public Double getLat() {
+        return lat;
+    }
+    
+    public void setLat(Double lat) {
+        this.lat = lat;
+    }
+
+     public Double getLng() {
+        return lng;
+    }
+    
+    public void setLng(Double lng) {
+        this.lng = lng;
+    }
+
+    public Set<String> getTypes() {
+    return types;
+    }
+
+    public void setTypes(Set<String> types) {
+        this.types = types;
+    }
+
+
+    
+}
