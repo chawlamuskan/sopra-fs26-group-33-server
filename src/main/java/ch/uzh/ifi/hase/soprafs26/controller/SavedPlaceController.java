14582f69b49ba483da2ddd439d71284e250a4b24
nsavas8 <nina.jael@gmx.ch>
2026-04-23 23:45:33 +0200

    Created endpoint to controller to save a place to a user (POST) #283

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/controller/SavedPlaceController.java
@@ -0,0 +1,62 @@
+package ch.uzh.ifi.hase.soprafs26.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import ch.uzh.ifi.hase.soprafs26.entity.SavedPlace;
+import ch.uzh.ifi.hase.soprafs26.entity.User;
+import ch.uzh.ifi.hase.soprafs26.rest.dto.SavedPlaceGetDTO;
+import ch.uzh.ifi.hase.soprafs26.rest.dto.SavedPlacePostDTO;
+import ch.uzh.ifi.hase.soprafs26.rest.mapper.DTOMapper;
+
+import ch.uzh.ifi.hase.soprafs26.service.SavedPlaceService;
+import ch.uzh.ifi.hase.soprafs26.service.UserService;
+
+
+@RestController
+public class SavedPlaceController {
+
+    private final SavedPlaceService savedPlaceService;
+    private final UserService userService;
+
+    SavedPlaceController(SavedPlaceService savedPlaceService, UserService userService) {
+        this.savedPlaceService = savedPlaceService;
+        this.userService = userService;
+        
+    }
+
+    @PostMapping("/users/{userId}/savedplaces")
+    @ResponseStatus(HttpStatus.CREATED)
+	@ResponseBody
+    public SavedPlaceGetDTO createSavedPlace(
+        @PathVariable Long userId, 
+        @RequestBody SavedPlacePostDTO savedPlacePostDTO, 
+        @RequestHeader(value = "Authorization", required = false) String token) {
+        
+        // check if the user is logged in and the token belongs to the user whose saved place is being set
+        User loggedInUser = userService.validateToken(token);
+		if (!loggedInUser.getId().equals(userId)) {
+			throw new ResponseStatusException(HttpStatus.FORBIDDEN, 
+				"You are not allowed to set other users' saved places.");
+		}
+        
+        SavedPlace savedPlace = DTOMapper.INSTANCE.convertSavedPlacePostDTOToEntity(savedPlacePostDTO);
+
+        SavedPlace createdSavedPlace = savedPlaceService.saveToUser(userId, savedPlace);
+
+        return DTOMapper.INSTANCE.convertEntityToSavedPlaceGetDTO(createdSavedPlace);
+    }
+    
+    
+}
