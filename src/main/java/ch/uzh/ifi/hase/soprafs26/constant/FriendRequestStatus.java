00ce3762972b2f42a8566718a5296814df5e688d
Yalini Sivapathasundaram <yalini.sivapathasundaram@uzh.ch>
2026-05-02 23:57:18 +0200

    uncomment Friend Request related code

--- src/main/java/ch/uzh/ifi/hase/soprafs26/constant/FriendRequestStatus.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/constant/FriendRequestStatus.java
@@ -1,6 +1,5 @@
-//package ch.uzh.ifi.hase.soprafs26.constant;
-//
-//public enum FriendRequestStatus {
-//	PENDING, ACCEPTED, DECLINED;
-//}
-//
+package ch.uzh.ifi.hase.soprafs26.constant;
+
+public enum FriendRequestStatus {
+	PENDING, ACCEPTED, DECLINED;
+}
