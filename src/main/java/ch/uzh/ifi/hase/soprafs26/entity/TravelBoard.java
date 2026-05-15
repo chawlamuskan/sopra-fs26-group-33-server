e80c2dd2dced4a6e038cf8c784e304f5e7c0e11a
Nadia Pandolfo <pan.nadia04@gmail.com>
2026-05-15 12:22:46 +0200

    closes #463 . Changes in the travel board entity and DTOs to save in the database bound geographical coordinates of the travel board location.

--- src/main/java/ch/uzh/ifi/hase/soprafs26/entity/TravelBoard.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/entity/TravelBoard.java
@@ -43,6 +43,18 @@ public class TravelBoard implements Serializable {
 
 	@Column(nullable = false)
 	private LocalDate dateCreated;
+	
+	@Column(nullable = true)
+	private Double latMin;
+
+	@Column(nullable = true)
+	private Double latMax;
+
+	@Column(nullable = true)
+	private Double lngMin;
+
+	@Column(nullable = true)
+	private Double lngMax;
     
     @ManyToMany
 	private List<User> members = new ArrayList<>();
@@ -132,6 +144,18 @@ public class TravelBoard implements Serializable {
 	public void setMembers(List<User> members) {
 		this.members = members;
 	}
+
+	public Double getLatMin() { return latMin; }
+	public void setLatMin(Double latMin) { this.latMin = latMin; }
+
+	public Double getLatMax() { return latMax; }
+	public void setLatMax(Double latMax) { this.latMax = latMax; }
+
+	public Double getLngMin() { return lngMin; }
+	public void setLngMin(Double lngMin) { this.lngMin = lngMin; }
+
+	public Double getLngMax() { return lngMax; }
+	public void setLngMax(Double lngMax) { this.lngMax = lngMax; }
 
 
 }
