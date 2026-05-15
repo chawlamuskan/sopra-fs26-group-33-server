5cfc4e2008f98dc7a0f17c59d24cb152cd21d679
Nadia Pandolfo <pan.nadia04@gmail.com>
2026-05-15 11:33:30 +0200

    Adjustment travel board service

--- src/main/java/ch/uzh/ifi/hase/soprafs26/service/TravelBoardService.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs26/service/TravelBoardService.java
@@ -179,7 +179,6 @@ public class TravelBoardService {
         board.getMembers().add(user);
         travelBoardRepository.save(board);
 
-        // ← log joining
         activityLogService.log(board, user, "joined the board");
     }
 }
